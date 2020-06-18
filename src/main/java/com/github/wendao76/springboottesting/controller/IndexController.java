5885b23df68c45a0c95920940b51ef994e9e2f09
wendao76 <tiger900721@163.com>
2020-06-18 15:14:52 +0800

    test

---
+++ src/main/java/com/github/wendao76/springboottesting/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.github.wendao76.springboottesting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wendao76
+ * @version 1.0
+ * @description 类描述信息
+ * @className IndexController
+ * @date 2020-6-18 15:05
+ */
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+	@GetMapping
+	public String home(){
+		return this.getClass().getCanonicalName();
+	}
+}
---
+++ src/test/java/com/github/wendao76/springboottesting/SpringbootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wendao76.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
