6f401243af75efdb9e61f92fe55a894b5e5404e2
wendao76 <tiger900721@163.com>
2020-06-19 10:23:32 +0800

    代码优化

--- src/test/java/com/github/wendao76/springboottesting/BaseMockTest.java
+++ src/test/java/com/github/wendao76/springboottesting/BaseMockTest.java
@@ -1,14 +1,8 @@
 package com.github.wendao76.springboottesting;
 
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -24,7 +18,6 @@ public abstract class BaseMockTest {
 	private WebApplicationContext wc;
 
 	protected MockMvc mockMvc;
-
 	@Before
 	public void beforeSetUp() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wc).build();
